9b0eb708f53837ddf4b50e0c1fc4df87ea7b5815
hrishikesh-madgundi <hrishibmadgundi@gmail.com>
2025-05-31 18:17:55 +0530

    Captain profile login and logout implemented

--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -17,4 +17,15 @@ router.post('/register', [
     captainController.registerCaptain
 )
 
+router.get('/login', [
+    router.post('/login', [
+        body('email').isEmail().withMessage('Invalid Email'),
+        body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+    ],
+        captainController.loginCaptain
+    )
+],
+    captainController.loginCaptain
+)
+
 module.exports = router;
